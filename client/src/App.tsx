0bb7616e518d8621dfde7b4078486ac35097f907
skvinderr <skvinderr@gmail.com>
2025-11-01 12:42:26 +0530

    refactor: move utility functions and add application structure files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { InteractiveGridPattern } from "./components/ui/interactive-grid-pattern";
-import { cn } from "./lib/utils";
+import { cn } from "./utils";
 import "./App.css";
 
 export default function App() {
